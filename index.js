6bd3fcd4e2afb98051db6913d123912a8dfd2fe0
aminariaz1 <riazamina990@gmail.com>
2024-11-30 00:05:53 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+/*export const hello=(i,j,k)=>{
+    return (i+j+k);
+}*/
+
+export const hi =(l,m,n)=>{
+    return(l*m*n);
+}
+
---
+++ script.js
@@ -0,0 +1,10 @@
+/*import { hello } from "./index.js";
+let okk = hello (20,30,90);
+console.log (okk);*/
+
+import {hi} from "./index.js";
+let okkk=hi(7,8,9);
+console.log(okkk);
+
+
+
